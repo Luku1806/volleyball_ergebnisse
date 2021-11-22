0f3ecafed39c7294b34bb43f23e86ca9571affa1
Lukas Reining <lukas.reining@codecentric.de>
2021-11-22 23:12:07 +0100

    Change to new domain name

--- lib/infrastructure/repositories/volleyballergebnisse/api_http_client.dart
+++ lib/infrastructure/repositories/volleyballergebnisse/api_http_client.dart
@@ -2,7 +2,7 @@ import 'package:http/http.dart' as http;
 
 class HandballErgebnisseApiHttpClient extends http.BaseClient {
   static const BASE_URL =
-      "https://volleyballergebnisse-functions.azurewebsites.net/api/v1";
+      "https://volleyball-ergebnisse.reiningapps.de/api/v1";
 
   @override
   Future<http.StreamedResponse> send(http.BaseRequest request) {
